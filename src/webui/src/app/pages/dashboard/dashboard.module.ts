0e188075cc356e2c69d997c83a883585086a0eb3
caglar bilgin <caglar.bilgin@sahabt.com>
2019-04-03 16:36:04 +0300

    header footer sidebar ve app layout eklendi

--- src/webui/src/app/pages/dashboard/dashboard.module.ts
+++ src/webui/src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard.rooting.module';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
